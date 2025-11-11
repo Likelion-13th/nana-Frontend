22b0eb7ffabf7c8330c3ac5502c3b4dc51e5dbac
kimyena123 <kimyena4930@kau.kr>
2025-11-11 21:59:33 +0900

    !

--- src/components/ToolBar.js
+++ src/components/ToolBar.js
@@ -1,60 +1,91 @@
 import React from "react";
 import "../styles/ToolBar.css";
-/*import { useCookies } from "react-cookie";*/
+import axios from "axios";
+import { useCookies } from "react-cookie";
 
+const ToolBar = ({ isLogin, onLoginChange }) => {
+  const [cookies, , removeCookie] = useCookies(["accessToken"]);
 
-const ToolBar = ({isLogin, onLoginChange}) => {
-    return (
-        <div className="toolbar-container">
-          <img
-            src={
-              isLogin
-                ? `${process.env.PUBLIC_URL}/icon/icon_logout.svg`
-                : `${process.env.PUBLIC_URL}/icon/icon_login.svg`
-            }
-            alt="login"
-            className="toolbar-icon"
-            onClick={handleLoginRedirect}
-          ></img>
-          <img
-            src={`${process.env.PUBLIC_URL}/icon/icon_recent.svg`}
-            alt="recent"
-            className="toolbar-icon"
-          ></img>
-          <img
-            src={`${process.env.PUBLIC_URL}/icon/icon_up.svg`}
-            alt="up"
-            className="toolbar-icon"
-            onClick={MoveToTop}
-          ></img>
-          <img
-            src={`${process.env.PUBLIC_URL}/icon/icon_down.svg`}
-            alt="down"
-            className="toolbar-icon"
-            onClick={MoveToBottom}
-          ></img>
-        </div>
-      );
-};
-
-const MoveToTop = () => {
+  const MoveToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
-};
+  };
 
-const MoveToBottom = () => {
+  const MoveToBottom = () => {
     window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
+  };
+
+  // 로그인/로그아웃 공용 클릭 핸들러
+  const handleAuthClick = async () => {
+    if (isLogin) {
+      // 🔐 로그아웃
+      try {
+        await axios.delete(
+          "http://sajang-dev-env.eba-cxzcfs22.ap-northeast-2.elasticbeanstalk.com/users/logout",
+          {
+            headers: {
+              Authorization: `Bearer ${cookies.accessToken || ""}`,
+              accept: "*/*",
+            },
+            withCredentials: true,
+          }
+        );
+
+        // 상태/쿠키 정리
+        onLoginChange(false);
+        removeCookie("accessToken", { path: "/" });
+
+        // 메인으로 이동
+        window.location.href = "/";
+      } catch (err) {
+        console.error("LOGOUT API 요청 실패", err);
+        alert("로그아웃 중 오류가 발생했습니다.");
+      }
+    } else {
+      // 🔑 로그인 (카카오 OAuth 시작)
+      const redirectUrl =
+        process.env.NODE_ENV === "development"
+          ? "http://localhost:3000"
+          : "https://nana-frontend.netlify.app/";
+
+      const oauthUrl =
+        "http://sajang-dev-env.eba-cxzcfs22.ap-northeast-2.elasticbeanstalk.com/oauth2/start/kakao" +
+        `?redirect_uri=${encodeURIComponent(redirectUrl)}`;
+
+      window.location.href = oauthUrl;
+    }
+  };
+
+  return (
+    <div className="toolbar-container">
+      <img
+        src={
+          isLogin
+            ? `${process.env.PUBLIC_URL}/icon/icon_logout.svg`
+            : `${process.env.PUBLIC_URL}/icon/icon_login.svg`
+        }
+        alt="login"
+        className="toolbar-icon"
+        onClick={handleAuthClick}
+      />
+      <img
+        src={`${process.env.PUBLIC_URL}/icon/icon_recent.svg`}
+        alt="recent"
+        className="toolbar-icon"
+      />
+      <img
+        src={`${process.env.PUBLIC_URL}/icon/icon_up.svg`}
+        alt="up"
+        className="toolbar-icon"
+        onClick={MoveToTop}
+      />
+      <img
+        src={`${process.env.PUBLIC_URL}/icon/icon_down.svg`}
+        alt="down"
+        className="toolbar-icon"
+        onClick={MoveToBottom}
+      />
+    </div>
+  );
 };
-
-const handleLoginRedirect = () => {
-  const redirectUrl =
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3000"
-      : "https://nana-frontend.netlify.app/"
-
-  const oauthUrl = "http://Sajang-dev-env.eba-cxzcfs22.ap-northeast-2.elasticbeanstalk.com/oauth2/start/kakao" +
-  `?redirect_uri=${encodeURIComponent(redirectUrl)}`;
-
-  window.location.href = oauthUrl;
-}
 
 export default ToolBar;
