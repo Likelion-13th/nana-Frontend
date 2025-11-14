7cd286e0015f3d550bc8adfb2e3fad704da58ffa
kimyena123 <kimyena4930@kau.kr>
2025-11-15 00:03:24 +0900

    프로필 API

--- src/pages/Mypage/Profile.js
+++ src/pages/Mypage/Profile.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Profile = () => {
+const Profile = ({ profileData }) => {
   const formatCurrency = (amount) => {
     return new Intl.NumberFormat('ko-KR').format(amount);
   };
@@ -8,22 +8,23 @@ const Profile = () => {
     <div className="profile-container">
         <div className="profile-section">
             <div className="profile-name">
-	            <span className="profile-realName">나나</span>
-	            님
+	            <span className="profile-realName">
+                {profileData?.usernickname ?? ""}
+              </span>님
 	          </div>
             <div className="profile-membership">[VVIP]회원</div>
         </div>
         <div className="profile-section">
             <div className="profile-stat-label">총 결제 금액</div>
             <div className="profile-stat-value">
-	            <span>{formatCurrency(100000)}</span>
+	            <span>{formatCurrency(profileData?.recentTotal)}</span>
 			        원
 			      </div>
         </div>
         <div className="profile-section">
             <div className="profile-stat-label">마일리지</div>
             <div className="profile-stat-value">
-	            <span>{formatCurrency(10000)}</span>
+	            <span>{formatCurrency(profileData?.maxMileage)}</span>
 	            원
 		        </div>
         </div>
