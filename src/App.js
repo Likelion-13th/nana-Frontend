be68867df72e4b84555ee4f510d5b47fef533847
kimyena123 <kimyena4930@kau.kr>
2025-11-16 01:46:28 +0900

    app.js 쿠키랑 isLogin이 자동으로 동기화

--- src/App.js
+++ src/App.js
@@ -1,59 +1,37 @@
-// src/App.js
-import "./App.css";
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { CookiesProvider, useCookies } from "react-cookie";
-import axios from "axios";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { CookiesProvider } from "react-cookie";
 
-import Home from "./pages/home/home";
-import MyPage from "./pages/Mypage/Mypage";
-import Diffuser from "./pages/ProductPage/Diffuser";
-import Perfume from "./pages/ProductPage/Perfume";
-import New from "./pages/ProductPage/New";
-import Footer from "./components/Footer";
+
 import Header from "./components/Header";
+import Footer from './components/Footer';
+import Home from './pages/home/home';
+import Mypage from './pages/Mypage/Mypage';
+import Diffuser from './pages/ProductPage/Diffuser';
+import Perfume from './pages/ProductPage/Perfume';
+import New from './pages/ProductPage/New';
 import ToolBar from "./components/ToolBar";
 
-// 전역 axios 설정 (다른 API는 기존대로 상대경로 사용)
-axios.defaults.baseURL = "/";
-axios.defaults.withCredentials = true;
-axios.defaults.validateStatus = (s) => s < 500;
 
-// 혹시 이전에 잘못 들어간 Accept 기본값이 있다면 제거
-if (axios.defaults.headers?.common?.Accept === "/") {
-  delete axios.defaults.headers.common.Accept;
-}
-
-function AppInner() {
+function App() {
   const [isLogin, setIsLogin] = useState(false);
-  const [cookies] = useCookies(["accessToken"]);
-
-  useEffect(() => {
-    setIsLogin(!!cookies.accessToken);
-  }, [cookies.accessToken]);
-
-  return (
-    <>
-      <Header />
-      <ToolBar isLogin={isLogin} onLoginChange={setIsLogin} />
-      <Routes>
-        <Route path="/" element={<Home onLoginChange={setIsLogin} />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/diffuser" element={<Diffuser />} />
-        <Route path="/perfume" element={<Perfume />} />
-        <Route path="/new" element={<New />} />
-      </Routes>
-      <Footer />
-    </>
-  );
-}
-
-export default function App() {
   return (
     <CookiesProvider>
       <Router>
-        <AppInner />
+        <Header />
+        <ToolBar isLogin={isLogin} onLoginChange={setIsLogin}/>
+        <Routes>
+          <Route path="/" element={<Home onLoginChange={setIsLogin}/>} />
+          <Route path="/mypage" element={<Mypage />} />
+          <Route path="/diffuser" element={<Diffuser />} />
+          <Route path="/perfume" element={<Perfume />} />
+          <Route path="/new" element={<New />} />
+        </Routes>
+        <Footer />
       </Router>
     </CookiesProvider>
+    
   );
 }
+
+export default App;
--- src/components/ToolBar.js
+++ src/components/ToolBar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "../styles/ToolBar.css";
-import  axios  from 'axios';
-import { useCookies } from 'react-cookie';
+import axios from "axios";
+import { useCookies } from "react-cookie";
 
-const ToolBar = ({isLogin, onLoginChange}) => {
-  const [cookies, removeCookie] = useCookies(["accessToken"]);
+const ToolBar = ({ isLogin, onLoginChange }) => {
+  // ✅ setCookie는 안 쓰니까 건너뛰고, 세 번째 값을 removeCookie로 받기
+  const [cookies, , removeCookie] = useCookies(["accessToken"]);
 
   const handleLogout = () => {
     axios
@@ -12,55 +13,61 @@ const ToolBar = ({isLogin, onLoginChange}) => {
         headers: {
           accept: "*/*",
           Authorization: `Bearer ${cookies.accessToken}`,
-      },
-    })
-    .then(() => {
-      onLoginChange(false);
-      removeCookie("accessToken", { path: "/"});
-    })
-    .catch((err) => {
-      console.log("LOGOUT API 요청 실패", err);
-    });
-  }
-    return(
-        <div className="toolbar-container">
-            <img
-              src={
-                isLogin 
-                ?`${process.env.PUBLIC_URL}/icon/icon_logout.svg`
-                :`${process.env.PUBLIC_URL}/icon/icon_login.svg`
-              }
-              alt="login"
-              className="toolbar-icon"
-              onClick ={isLogin ? handleLogout : handleloginRedirect}
-            ></img>
-            <img
-              src={`${process.env.PUBLIC_URL}/icon/icon_recent.svg`}
-              alt="recent"
-              className="toolbar-icon"
-            ></img>  
-            <img
-              src={`${process.env.PUBLIC_URL}/icon/icon_up.svg`}
-              alt="up"
-              className="toolbar-icon"
-              onClick={MoveToTop}
-            ></img>  
-            <img
-              src={`${process.env.PUBLIC_URL}/icon/icon_down.svg`}
-              alt="down"
-              className="toolbar-icon"
-              onClick={MoveToBottom}
-            ></img>  
-        </div>
-    );
+        },
+      })
+      .then(() => {
+        // ✅ 서버 로그아웃 성공했으니 프론트 상태도 로그아웃
+        removeCookie("accessToken", { path: "/" });
+        onLoginChange(false);
+      })
+      .catch((err) => {
+        console.log("LOGOUT API 요청 실패", err);
+        // ✅ 서버에서 401 떠도 이미 서버 쪽은 로그아웃 상태일 수 있으니까
+        // 프론트에서는 그냥 강제 로그아웃 처리해도 됨
+        removeCookie("accessToken", { path: "/" });
+        onLoginChange(false);
+      });
+  };
+
+  return (
+    <div className="toolbar-container">
+      <img
+        src={
+          isLogin
+            ? `${process.env.PUBLIC_URL}/icon/icon_logout.svg`
+            : `${process.env.PUBLIC_URL}/icon/icon_login.svg`
+        }
+        alt="login"
+        className="toolbar-icon"
+        onClick={isLogin ? handleLogout : handleloginRedirect}
+      ></img>
+      <img
+        src={`${process.env.PUBLIC_URL}/icon/icon_recent.svg`}
+        alt="recent"
+        className="toolbar-icon"
+      ></img>
+      <img
+        src={`${process.env.PUBLIC_URL}/icon/icon_up.svg`}
+        alt="up"
+        className="toolbar-icon"
+        onClick={MoveToTop}
+      ></img>
+      <img
+        src={`${process.env.PUBLIC_URL}/icon/icon_down.svg`}
+        alt="down"
+        className="toolbar-icon"
+        onClick={MoveToBottom}
+      ></img>
+    </div>
+  );
 };
 
 const MoveToTop = () => {
-  window.scrollTo({top:0, behavior: "smooth"});
+  window.scrollTo({ top: 0, behavior: "smooth" });
 };
 
 const MoveToBottom = () => {
-  window.scrollTo({top:document.body.scrollHeight, behavior: "smooth"});
+  window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
 };
 
 const handleloginRedirect = () => {
@@ -70,12 +77,10 @@ const handleloginRedirect = () => {
       : "https://nana-frontend.netlify.app/";
 
   const oauthUrl =
-   "http://sajang-dev-env.eba-cxzcfs22.ap-northeast-2.elasticbeanstalk.com/oauth2/start/kakao" + 
-   `?redirect_uri=${encodeURIComponent(redirectUrl)}`;
+    "http://sajang-dev-env.eba-cxzcfs22.ap-northeast-2.elasticbeanstalk.com/oauth2/start/kakao" +
+    `?redirect_uri=${encodeURIComponent(redirectUrl)}`;
 
   window.location.href = oauthUrl;
 };
-
-
 
 export default ToolBar;
