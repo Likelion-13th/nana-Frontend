cfb3a4243ccd804b4af248c5ee669297f8af2d3a
kimyena123 <kimyena4930@kau.kr>
2025-11-16 01:28:07 +0900

    리다이렉트 수정

--- src/components/ToolBar.js
+++ src/components/ToolBar.js
@@ -1,94 +1,81 @@
-// src/components/ToolBar.jsx
 import React from "react";
 import "../styles/ToolBar.css";
-import axios from "axios";
-import { useCookies } from "react-cookie";
+import  axios  from 'axios';
+import { useCookies } from 'react-cookie';
 
-const ToolBar = ({ isLogin, onLoginChange }) => {
-  const [cookies, , removeCookie] = useCookies(["accessToken"]);
+const ToolBar = ({isLogin, onLoginChange}) => {
+  const [cookies, removeCookie] = useCookies(["accessToken"]);
 
-  const MoveToTop = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  };
-
-  const MoveToBottom = () => {
-    window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
-  };
-
-  // -------------------------------
-  // 로그인 리다이렉트 (카카오 OAuth)
-  // -------------------------------
-  const handleLoginRedirect = () => {
-    const oauthUrl =
-      "http://sajang-dev-env.eba-cxzcfs22.ap-northeast-2.elasticbeanstalk.com/oauth2/start/kakao";
-
-    window.location.href = oauthUrl;
-  };
-  //   // 개발/배포에 따라 돌아올 프론트 주소 분기
-  //   const redirect_url =
-  //     process.env.NODE_ENV === "development"
-  //       ? "http://localhost:3000"
-  //       : "https://nana-frontend.netlify.app";
-
-  //   // ★ 친구 코드 패턴 그대로: EB는 http 로 호출
-  //   const oauthUrl =
-  //     "http://sajang-dev-env.eba-cxzcfs22.ap-northeast-2.elasticbeanstalk.com/oauth2/start/kakao" +
-  //     `?redirect_uri=${encodeURIComponent(redirect_url)}`;
-
-  //   window.location.href = oauthUrl;
-  // };
-
-  // -------------------------------
-  // 로그아웃 처리
-  // -------------------------------
   const handleLogout = () => {
     axios
       .delete("/users/logout", {
         headers: {
           accept: "*/*",
           Authorization: `Bearer ${cookies.accessToken}`,
-        },
-      })
-      .then(() => {
-        onLoginChange(false);
-        removeCookie("accessToken", { path: "/" });
-      })
-      .catch((err) => {
-        console.log("LOGOUT API 요청 실패:", err);
-      });
-  };
-
-  return (
-    <div className="toolbar-container">
-      <img
-        src={
-          isLogin
-            ? `${process.env.PUBLIC_URL}/icon/icon_logout.svg`
-            : `${process.env.PUBLIC_URL}/icon/icon_login.svg`
-        }
-        alt="login"
-        className="toolbar-icon"
-        onClick={isLogin ? handleLogout : handleLoginRedirect}
-      />
-      <img
-        src={`${process.env.PUBLIC_URL}/icon/icon_recent.svg`}
-        alt="recent"
-        className="toolbar-icon"
-      />
-      <img
-        src={`${process.env.PUBLIC_URL}/icon/icon_up.svg`}
-        alt="up"
-        className="toolbar-icon"
-        onClick={MoveToTop}
-      />
-      <img
-        src={`${process.env.PUBLIC_URL}/icon/icon_down.svg`}
-        alt="down"
-        className="toolbar-icon"
-        onClick={MoveToBottom}
-      />
-    </div>
-  );
+      },
+    })
+    .then(() => {
+      onLoginChange(false);
+      removeCookie("accessToken", { path: "/"});
+    })
+    .catch((err) => {
+      console.log("LOGOUT API 요청 실패", err);
+    });
+  }
+    return(
+        <div className="toolbar-container">
+            <img
+              src={
+                isLogin 
+                ?`${process.env.PUBLIC_URL}/icon/icon_logout.svg`
+                :`${process.env.PUBLIC_URL}/icon/icon_login.svg`
+              }
+              alt="login"
+              className="toolbar-icon"
+              onClick ={isLogin ? handleLogout : handleloginRedirect}
+            ></img>
+            <img
+              src={`${process.env.PUBLIC_URL}/icon/icon_recent.svg`}
+              alt="recent"
+              className="toolbar-icon"
+            ></img>  
+            <img
+              src={`${process.env.PUBLIC_URL}/icon/icon_up.svg`}
+              alt="up"
+              className="toolbar-icon"
+              onClick={MoveToTop}
+            ></img>  
+            <img
+              src={`${process.env.PUBLIC_URL}/icon/icon_down.svg`}
+              alt="down"
+              className="toolbar-icon"
+              onClick={MoveToBottom}
+            ></img>  
+        </div>
+    );
 };
+
+const MoveToTop = () => {
+  window.scrollTo({top:0, behavior: "smooth"});
+};
+
+const MoveToBottom = () => {
+  window.scrollTo({top:document.body.scrollHeight, behavior: "smooth"});
+};
+
+const handleloginRedirect = () => {
+  const redirectUrl =
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : "https://nana-frontend.netlify.app/";
+
+  const oauthUrl =
+   "http://sajang-dev-env.eba-cxzcfs22.ap-northeast-2.elasticbeanstalk.com/oauth2/start/kakao" + 
+   `?redirect_uri=${encodeURIComponent(redirectUrl)}`;
+
+  window.location.href = oauthUrl;
+};
+
+
 
 export default ToolBar;
